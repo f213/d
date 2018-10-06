b57eab1aab091d04e5c15812a2a2dc0d5c75f048
Fedor Borshev <f@f213.in>
2018-10-06 11:45:27 +0300

    Better syntax for complex run arguments

---
+++ test/tests_args_flattening.py
@@ -0,0 +1,23 @@
+import pytest
+
+from d import flatten_args
+
+
+@pytest.mark.parametrize('args, expected', [
+    ([['docker', 'build'], '-t'], ['docker', 'build', '-t']),
+    ([['docker', 'build'], '-t', ['org/img', '-f', 'Dockerfile']], ['docker', 'build', '-t', 'org/img', '-f', 'Dockerfile']),
+    (['-t', '-i'], ['-t', '-i']),
+    (['-t', 5], ['-t', '5']),
+    ([['-t', '-i']], ['-t', '-i']),
+])
+def test(args, expected):
+    assert flatten_args(args) == expected
+
+
+@pytest.mark.parametrize('args', [
+    ['-t', None],
+    ['-t', lambda: 'fail'],
+])
+def test_error(args):
+    with pytest.raises(TypeError):
+        flatten_args(args)
